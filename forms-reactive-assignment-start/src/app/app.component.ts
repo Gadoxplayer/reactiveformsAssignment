0c73611c3eaf9bb89f01b4940b6bdaf5ad63c5dc
EduardoCampo <eduardocampo91@gmail.com>
2024-01-03 23:37:05 -0500

    update

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { log } from 'console';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  projectStatus = ['Stable', 'Critical', 'Finished'];
+  formProject: FormGroup;
+
+  ngOnInit(): void {
+      this.formProject = new FormGroup({
+        'projectData': new FormGroup({
+          'projectName': new FormControl(null, Validators.required),
+          'email': new FormControl(null, [Validators.required, Validators.email]),
+          'projectStatus': new FormArray([])
+        })
+      })
+  }
+
+  onSubmit() {
+    console.log(this.formProject);
+  }
 }
